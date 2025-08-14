c7b09e7959824dd978839a60cc1ed1325e3ba564
rosiilva <roberta.silva@colab.re>
2025-08-14 12:45:06 -0300

    correcting TC09

--- cypress/e2e/board/tarefas.cy.js
+++ cypress/e2e/board/tarefas.cy.js
@@ -1,27 +1,25 @@
 describe('Gerenciamento de Tarefas', () => {
+  const novaColuna = 'Nova coluna'
+  const novaTask = 'Nova tarefa'
+
   beforeEach(() => {
     cy.visit('https://kanban-dusky-five.vercel.app')
+    cy.newColumn(novaColuna)
+
   })
 
   it('TC07 - Adicionar tarefa na coluna existente', () => {
-    cy.newColumn('Nova coluna')
-    cy.newTask('Nova tarefa', 'Nova coluna')
+    cy.newTask(novaTask, novaColuna)
 })
 
-  it('TC08 - Criar nova tarefa com nome vazio', () => {
-    cy.newColumn('Nova coluna')
-    cy.newTask('', 'Nova coluna')
-
-    })
-})
+//   it('TC08 - Criar nova tarefa com nome vazio', () => {
+//     cy.newTask('', novaColuna)
+// })
 
   it('TC09 - Remover tarefa da coluna existente', () => {
-    cy.newColumn('Nova coluna')
-    cy.newTask('Nova tarefa', 'Nova coluna')
+    cy.newTask(novaTask, novaColuna)
 
-   
-    cy.contains('Nova tarefa').parent().within(() => {
-    cy.get('.trash').click()
+    cy.contains(novaTask).get('svg.trash').last().click({ force: true })   
+    cy.contains(novaTask).should('not.exist')
   })
-    cy.contains('Nova tarefa').should('not.exist')
-  })
+})
